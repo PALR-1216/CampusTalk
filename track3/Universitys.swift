d9689b07b8f750d232f1f4104e4d9a431b5ca1ec
Pedro Alejandro LR <pedroalejandrolr@Senpai.local>
2023-09-18 01:18:42 -0400

    2 commit

---
+++ track3/Universitys.swift
@@ -0,0 +1,20 @@
+//
+//  Universitys.swift
+//  track3
+//
+//  Created by Pedro Alejandro LR on 9/17/23.
+//
+
+import SwiftUI
+
+struct Universitys: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Universitys_Previews: PreviewProvider {
+    static var previews: some View {
+        Universitys()
+    }
+}
